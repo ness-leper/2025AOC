a0741e4415af5c8d8b9ef7b7b7c28faa6e742642
ness-leper <nmonforte@gmail.com>
2026-03-10 21:41:38 -0400

    scaffold for picking the process

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/ness-leper/2025AOC/day1"
+)
+
+func main() {
+	dayPtr := flag.String("day", "unset", "The day that is being run")
+	partPtr := flag.String("part", "unset", "The part of the day we are running")
+	filePtr := flag.String("file", "unset", "The file to read")
+
+	// This parses the flags from the command line
+	flag.Parse()
+
+	fmt.Println(*dayPtr, *partPtr, *filePtr)
+
+	switch *dayPtr {
+	case "1":
+		switch *partPtr {
+		case "1":
+			day1.Part1(filePtr)
+		case "2":
+			fmt.Println("Not implemented yet")
+		}
+	}
+}
