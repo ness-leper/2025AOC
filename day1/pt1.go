183705260ce910edb1fba87bb69f9ad5a42605fa
ness-leper <nmonforte@gmail.com>
2026-03-10 22:18:37 -0400

    decoding the instructions

--- day1/pt1.go
+++ day1/pt1.go
@@ -1,8 +1,43 @@
 package day1
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	readfile "github.com/ness-leper/2025AOC/ReadFile"
+)
 
 func Part1(filename *string) {
-	fmt.Println("This is part1!")
-	fmt.Printf("We are using file: %s", *filename)
+	// dial starts at 50
+	dial := 50
+	password := 0
+
+	file := strings.SplitSeq(string(readfile.ReadFile(filename)), "\n")
+
+	for v := range file {
+		if len(v) <= 0 {
+			continue
+		}
+		direction := v[0:1]
+		move, _ := strconv.Atoi(v[1:])
+		rotate := -1
+		if direction == "R" {
+			rotate = 1
+		}
+		for range move {
+			dial = dial + rotate
+			if dial == -1 {
+				dial = 99
+			}
+			if dial == 100 {
+				dial = 0
+			}
+		}
+		if dial == 0 {
+			password += 1
+		}
+	}
+
+	fmt.Printf("The password is %d", password)
 }
